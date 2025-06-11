16c572c1958d39d4c6dc6d78600a749412c86fc9
TESLA-SATI <pro100ca@gmail.com>
2025-06-11 20:05:40 +0200

    Update main.py
    fix#4
    
    Signed-off-by: TESLA-SATI <pro100ca@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-#!/usr/local/bin/python3.10
-# -*- coding: utf-8 -*-
-import re
-import sys
-from aixblock_core.server import main
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv("export/vuln_scores.csv")
+
+app = dash.Dash(__name__)
+app.layout = html.Div([
+    html.H1("Scoring podatności CVSS + AI"),
+    dcc.Graph(
+        figure={
+            'data': [
+                {
+                    'x': df["id"],
+                    'y': df["combined_score"],
+                    'type': 'bar',
+                    'name': 'Combined Score'
+                }
+            ],
+            'layout': {
+                'title': 'Wyniki scoringu dla podatności'
+            }
+        }
+    )
+])
+
 if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    main()
+    app.run_server(debug=True)
